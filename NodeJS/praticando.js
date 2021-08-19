5e2b6559d7f102546533f4c4c7db6cb96e5304c7
Arthur Saldanha <30752069+arthursaldanha@users.noreply.github.com>
2021-08-19 20:21:24 -0300

    Praticando com Node.JS

---
+++ NodeJS/praticando.js
@@ -0,0 +1,64 @@
+// Incluindo uma biblioteca
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const queryString = require('query-string');
+
+// Definição de endereço / URL
+const hostname = '127.0.0.1'; // localhost
+const port = 3000;
+
+// Implementação da regra de negócio
+const server = http.createServer((req, res) => {
+
+   var resposta;
+   const urlparse = url.parse(req.url, true);
+   const params = queryString.parse(urlparse.search); // Receber informações do usuário
+
+   // Criar um usuário - Atualizar um usuário
+   if (urlparse.pathname == '/criar-atualizar-usuario') {
+      
+      // Salvar informações
+      fs.writeFile('./users/' + params.id + '.txt', JSON.stringify(params), function (err) {
+         if (err) throw err;
+         console.log('Saved!');
+         resposta = "Usuario criado com sucesso";
+
+         res.statusCode = 200;
+         res.setHeader('Content-Type', 'text/plain');
+         res.end(resposta);
+      });
+   } 
+   // Selecionar um usuário
+   else if (urlparse.pathname == '/selecionar-usuario') { 
+      fs.readFile('./users/' + params.id + '.txt', function (err, data) {
+         resposta = data
+         console.log(data);
+
+         res.statusCode = 200;
+         res.setHeader('Content-Type', 'application/json');
+         res.end(resposta);
+      });
+   }
+   // Remover um usuário
+   else if (urlparse.pathname == '/remover-usuario') { 
+      fs.unlink('./users/' + params.id + '.txt', function (err) {
+         console.log('File deleted!');
+
+         resposta = err ? "Usuario nao encontrado." : "Usuario removido.";
+
+         res.statusCode = 200;
+         res.setHeader('Content-Type', 'text/plain');
+         res.end(resposta);
+       }); 
+   }
+});
+
+
+server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+});
+// http://localhost:3000/criar-atualizar-usuario?nome=Arthur&idade=19&id=1
+// http://localhost:3000/criar-atualizar-usuario?nome=Arthur&idade=21&id=1
+// http://localhost:3000/selecionar-usuario?id=1
+// http://localhost:3000/remover-usuario?id=1
